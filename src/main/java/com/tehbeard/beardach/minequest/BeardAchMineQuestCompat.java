27e61afce73c1b819b506634c05c5d9abaefdbf9
James <beardyjames@live.co.uk>
2013-11-15 17:29:31 +0000

    Initial commit

---
+++ src/main/java/com/tehbeard/beardach/minequest/BeardAchMineQuestCompat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tehbeard.beardach.minequest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author James
+ */
+public class BeardAchMineQuestCompat extends JavaPlugin{
+    
+}
